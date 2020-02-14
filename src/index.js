f7c646c3f0b7a2729c88628d9357d67d5f4a8d11
Timothee Reynaud <reynaudtimothee@gmail.com>
2020-02-14 18:35:53 +0000

    removed cors

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ const db = require('./db');
 const server = createServer();
 
 server.start({
-    cors: {
-        credentials: true,
-        origin: process.env.FRONTEND_URL
-    }
+    // cors: {
+    //     credentials: true,
+    //     origin: process.env.FRONTEND_URL
+    // }
 }, deets => {
     console.log(`Server is now running on port http:/localhost:${deets.port}`)
 })
