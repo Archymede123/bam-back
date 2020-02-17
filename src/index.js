9c280cd206bd77d9adabeca1046f5fb60fc6f20a
Timothee Reynaud <reynaudtimothee@gmail.com>
2020-02-17 12:29:20 +0000

    cors

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ const db = require('./db');
 
 const server = createServer();
 
-server.start({
-    // cors: {
-    //     credentials: true,
-    //     origin: process.env.FRONTEND_URL
-    // }
-}, deets => {
-    console.log(`Server is now running on port http:/localhost:${deets.port}`)
-})
+server.start(
+    {
+        cors: {
+            credentials: true,
+            origin: process.env.FRONTEND_URL
+        }
+    },
+    deets => {
+        console.log(`Server is now running on port http:/localhost:${deets.port}`)
+    }
+)
 
